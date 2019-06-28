4dce4a66980262cd866b496451bd2a642523361b
Emmanuel Adesile <emma2adesile@gmail.com>
2019-06-28 11:09:24 +0100

    update font awesome icon for video rating

--- dist/src.80dfb952.js
+++ dist/src.80dfb952.js
@@ -41828,7 +41828,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55700" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50653" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
